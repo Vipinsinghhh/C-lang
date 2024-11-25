1ca140902faa1d023da93bb609c7411da3ed683c
vipinsinghhh <vs069056@gmail.com>
2024-11-25 23:44:06 +0530

    program to check is a number is dvisible by 2 or not

---
+++ Chapter-2/PQ1.c
@@ -0,0 +1,11 @@
+// program to check is a number is dvisible by 2 or not
+
+#include<stdio.h>
+
+int main(){
+    int x;
+    printf("enter the no:");
+    scanf("%d ",&x);
+    printf("%d\n",x % 2 ==0);
+    return 0;
+}
