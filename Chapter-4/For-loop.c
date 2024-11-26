4b7dd7d71335d8e9a288f0e9ce7828a7d40b9e82
vipinsinghhh <vs069056@gmail.com>
2024-11-26 22:54:00 +0530

    program of print the numbers from 1 to 10 using for loop

---
+++ Chapter-4/For-loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    for(int i=0; i<=10; i=i+1){
+    printf("%d\n",i);
+    }
+    return 0;
+}
+ // print 1 to 10 no. in decreasing order 
+
+  #include<stdio.h>
+
+ int main(){
+    for(int i=10; i>=1; i--){
+    printf("%d\n",i);
+    }
+    return 0;
+    
+ }
