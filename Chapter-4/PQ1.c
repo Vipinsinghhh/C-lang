da1ceb7274237a5b8d230e67d77604d8e3c26b95
vipinsinghhh <vs069056@gmail.com>
2024-11-26 23:04:05 +0530

    print the bo. 0 to n, if n is given by user

---
+++ Chapter-4/PQ1.c
@@ -0,0 +1,16 @@
+// write the number from 0 to n, if n is given by user 
+
+#include<stdio.h>
+
+int main(){
+    int n;
+    printf("enter the number :");
+    scanf("%d",&n);
+
+    int i=0;
+    while (i<=n){
+        printf("%d\n",i);
+        i++;
+    }
+    return 0;
+}
