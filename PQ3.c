48e543e35e90e96a4b68c34bc831caafbd78127d
vipinsinghhh <vs069056@gmail.com>
2024-11-26 21:32:08 +0530

    program of sum of two numbers

---
+++ PQ3.c
@@ -0,0 +1,14 @@
+// program of adding 2 numbers
+
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the value of a");
+    scanf("%d",&a);
+
+    printf("enter the value of b");
+    scanf("%d",&b);
+
+    printf("sum of a and b is %d",a+b);
+}
