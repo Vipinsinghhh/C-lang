50e8f081c20733aa4f3f50950ab7719968558037
vipinsinghhh <vs069056@gmail.com>
2024-11-26 22:58:19 +0530

    Added while loop program

---
+++ Chapter-4/while_loop.c
@@ -0,0 +1,24 @@
+// print 1 to 5 no. by using while loop
+
+#include<stdio.h>
+
+int main(){
+    int i=1;
+    while(i<=5){
+        printf("%d\n",i);
+        i++;
+    }
+}
+
+// print hello world in loop 
+
+
+#include<stdio.h>
+
+int main(){
+    int i=1;
+    while(i<=5){
+        printf("Hello world\n");
+        i++;
+    }
+}
