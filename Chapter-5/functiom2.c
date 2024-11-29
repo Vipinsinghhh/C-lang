42588cc9d10ad187341bab255940d14cd1abe7fd
vipinsinghhh <vs069056@gmail.com>
2024-11-29 20:19:08 +0530

    added a exammple code of function

---
+++ Chapter-5/functiom2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void printhello();
+
+void printhello(){
+    printf("HELLO\n");
+    printf("MY NAME IS VIPIN\n");
+}
+
+int main(){
+
+    printhello();
+    printhello();
+    printhello();
+    printhello();
+    printhello();
+
+    return 0;
+}
