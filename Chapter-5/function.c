7e03ac8d0c918908344225c0273950bf915cd3c2
vipinsinghhh <vs069056@gmail.com>
2024-11-29 20:04:21 +0530

    added example code of how function works and a syntax of a function

---
+++ Chapter-5/function.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+//  declaration / protype
+
+void printhello();
+
+//function declaration
+
+void printhello(){
+    printf("hello!");
+}
+
+int main(){
+
+    printhello();  //function call
+
+    return 0;
+}
