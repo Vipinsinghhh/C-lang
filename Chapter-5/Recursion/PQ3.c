cb4899a1a102d113f7cab50726aff59d06e7b19d
vipinsinghhh <vs069056@gmail.com>
2024-12-02 01:38:50 +0530

    write a function to convert celsius to fahrenhiet

---
+++ Chapter-5/Recursion/PQ3.c
@@ -0,0 +1,16 @@
+//write a function to convert celsius to fahrenhiet
+//using first written type of functon
+
+#include<stdio.h>
+
+float convertTemp(float celsius);
+
+int main(){
+    convertTemp(37);
+    return 0;
+}
+float convertTemp(float celsius){
+    float far = celsius*(9.0/5.0)+32;
+    printf("far : %f",far);
+    return far;
+}
