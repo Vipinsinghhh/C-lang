1dd0263abe190a47ce92477a33439ceff45f2505
vipinsinghhh <vs069056@gmail.com>
2025-02-06 18:40:54 +0530

    added a code of pointer to pointer adress storing

---
+++ Chapter-6/Pointer_to_pointer.c
@@ -0,0 +1,13 @@
+// basic code of pointr to pointer 
+
+#include<stdio.h>
+
+int main(){
+    float price = 100.00;
+    float *ptr = &price;
+    float **pptr = &ptr;
+
+    printf("%f\n",**pptr);
+
+    return 0;
+}
