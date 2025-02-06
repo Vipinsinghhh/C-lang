e3350a2bf582f1f83dfca67faf3240e6202cfbe8
vipinsinghhh <vs069056@gmail.com>
2025-02-06 19:40:01 +0530

    PQ- will the adress output be same

--- Chapter-6/PQ4.c
+++ Chapter-6/PQ4.c
@@ -1,5 +1,7 @@
 // will the adress output be same?
 
+// call by value
+
 #include<stdio.h>
 
 void printAddress(int n);
@@ -13,4 +15,18 @@ int main(){
 void printAddress(int n){
     printf("%p\n",&n);
 }
+//call by reference
 
+#include<stdio.h>
+
+void printAddress(int *n);
+
+int main(){
+    int n=4;
+    printAddress(&n);
+    printf("%p\n",&n);
+    return 0;
+}
+void printAddress(int *n){
+    printf("%p\n",&n);
+}
