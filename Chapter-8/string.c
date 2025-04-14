08615f4c2e78fac56579c7615c4923224bd1116b
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-04-15 00:38:27 +0530

    added a basic code of string

---
+++ Chapter-8/string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    char name[]={ 'v', 'i', 'p', 'i', 'n', '\0'};
+
+    char surname[]= "Singh";
+
+    printf("%s\n",name);
+    printf("%s\n",surname);
+
+    return 0;
+}
