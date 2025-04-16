4ed75a31a4f2be334035f27df3052feaa10afd10
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-04-16 20:36:42 +0530

    added a code of string where i used gets and puts function of string

---
+++ Chapter-8/PQ2.1.c
@@ -0,0 +1,16 @@
+// If you want to print a sentence then use gets and puts function of string
+
+#include<stdio.h>
+
+int main(){
+     
+    char Fullname[100];
+
+    printf("Enter your full name:");
+    gets (Fullname);
+    puts (Fullname);
+
+    return 0;
+
+
+}
