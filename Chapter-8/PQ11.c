223d4421dd90a8777f8a57aa567636fe5d6cbeb2
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-04-20 00:16:29 +0530

    program to print the highest frequency character in a string

---
+++ Chapter-8/PQ11.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str[]="aaaabbbbbbbbccd";
+    int freq[256]={0};
+
+    for(int i=0;str[i]!=0;i++){
+        freq[str[i]]++;
+    }
+
+    int max=0;
+
+    char ch;
+
+    for(int i=0;i<256;i++){
+        if(freq[i]>max){
+            max=freq[i];
+            ch = i;
+        }
+    }
+    printf("highest frequency character is:%c",ch);
+
+    return 0;
+}
