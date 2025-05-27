324e9de4353118eb6de400066277d81f3aa1a623
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-05-28 01:29:09 +0530

    added a practice code of file

---
+++ Chapter-10/PQ5.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    FILE *fptr;
+    fptr = fopen("test.txt","r");
+
+    int a;
+    int b;
+    
+    fscanf(fptr, "%d", &a);
+    fscanf(fptr, "%d", &b);
+
+    fclose(fptr);
+    
+    fptr = fopen("test.txt", "w");
+
+    fprintf(fptr, "%d", a+b);
+
+    fclose(fptr);
+    return 0;
+}
