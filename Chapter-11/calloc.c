12d9636da11905c8af1898b2df0f674c252e119c
Vipinsinghhh <169113380+Vipinsinghhh@users.noreply.github.com>
2025-05-29 19:49:05 +0530

    added a code of dynamic memory allocation by using calloc function

---
+++ Chapter-11/calloc.c
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+#include<stdlib.h>
+
+int main(){
+    float *ptr;
+    ptr = (float *)calloc(5,sizeof(float));
+
+    for(int i=0;i<5;i++){
+        printf("%f\n",ptr[i]);
+    }
+    return 0;
+}
